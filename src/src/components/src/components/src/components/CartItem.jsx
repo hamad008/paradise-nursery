e7934ca55eff1895b7f5064230024c9109c9a326
hamad008 <146463448+hamad008@users.noreply.github.com>
2026-04-22 20:19:05 +0400

    Create CartItem.jsx

---
+++ src/src/components/src/components/src/components/CartItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CartItem() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <p>Total: $0</p>
+      <button>Checkout (Coming Soon)</button>
+    </div>
+  );
+}
+
+export default CartItem;
